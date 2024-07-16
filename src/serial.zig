84e7033ecad4e3bdc1f93512083f1b2b7cdf11f8
Ben Jordan <ben@poecoh.com>
2024-07-16 15:17:51 -0400

    placed windows config in os specific file

--- src/serial.zig
+++ src/serial.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const builtin = @import("builtin");
-const MAXDWORD = std.math.maxInt(std.os.windows.DWORD);
 const native_os = @import("builtin").os.tag;
 const serial = @This();
 
@@ -117,60 +116,18 @@ const CBAUD = 0o000000010017; //Baud speed mask (not in POSIX).
 const CMSPAR = 0o010000000000;
 const CRTSCTS = 0o020000000000;
 
-const VTIME = 5;
-const VMIN = 6;
-const VSTART = 8;
-const VSTOP = 9;
-
 /// This function configures a serial port with the given config.
 /// `port` is an already opened serial port, on windows these
 /// are either called `\\.\COMxx\` or `COMx`, on unixes the serial
 /// port is called `/dev/ttyXXX`.
 pub fn configureSerialPort(port: std.fs.File, config: SerialConfig) !void {
     switch (builtin.os.tag) {
-        .windows => {
-            var dcb = std.mem.zeroes(DCB);
-            dcb.DCBlength = @sizeOf(DCB);
-
-            if (GetCommState(port.handle, &dcb) == 0)
-                return error.WindowsError;
-
-            // std.log.err("{s} {s}", .{ dcb, flags });
-
-            dcb.BaudRate = config.baud_rate;
-
-            dcb.flags = @bitCast(DCBFlags{
-                .fParity = config.parity != .none,
-                .fOutxCtsFlow = config.handshake == .hardware,
-                .fOutX = config.handshake == .software,
-                .fInX = config.handshake == .software,
-                .fRtsControl = @as(u2, if (config.handshake == .hardware) 1 else 0),
-            });
-
-            dcb.wReserved = 0;
-            dcb.ByteSize = @intFromEnum(config.word_size);
-            dcb.Parity = switch (config.parity) {
-                .none => @as(u8, 0),
-                .even => @as(u8, 2),
-                .odd => @as(u8, 1),
-                .mark => @as(u8, 3),
-                .space => @as(u8, 4),
-            };
-            dcb.StopBits = switch (config.stop_bits) {
-                .one => @as(u2, 0),
-                .two => @as(u2, 2),
-            };
-            dcb.XonChar = 0x11;
-            dcb.XoffChar = 0x13;
-            dcb.wReserved1 = 0;
-
-            if (SetCommState(port.handle, &dcb) == 0)
-                return error.WindowsError;
-            var timeout = COMMTIMEOUTS.init(config.timeout);
-            if (SetCommTimeouts(port.handle, &timeout) == 0)
-                return error.WindowsError;
-        },
+        .windows => try serial.wConfigure(port, config),
         else => {
+            const VTIME = 5;
+            const VMIN = 6;
+            const VSTART = 8;
+            const VSTOP = 9;
             var settings = try std.posix.tcgetattr(port.handle);
 
             settings.iflag = .{};
@@ -234,67 +191,6 @@ pub const ControlPins = struct {
     rts: ?bool = null,
     dtr: ?bool = null,
 };
-
-const DCBFlags = packed struct(u32) {
-    fBinary: bool = true, // u1
-    fParity: bool = false, // u1
-    fOutxCtsFlow: bool = false, // u1
-    fOutxDsrFlow: bool = false, // u1
-    fDtrControl: u2 = 1, // u2
-    fDsrSensitivity: bool = false, // u1
-    fTXContinueOnXoff: bool = false, // u1
-    fOutX: bool = false, // u1
-    fInX: bool = false, // u1
-    fErrorChar: bool = false, // u1
-    fNull: bool = false, // u1
-    fRtsControl: u2 = 0, // u2
-    fAbortOnError: bool = false, // u1
-    fDummy2: u17 = 0, // u17
-};
-
-/// Configuration for the serial port
-///
-/// Details: https://learn.microsoft.com/es-es/windows/win32/api/winbase/ns-winbase-dcb
-const DCB = extern struct {
-    DCBlength: std.os.windows.DWORD,
-    BaudRate: std.os.windows.DWORD,
-    flags: u32,
-    wReserved: std.os.windows.WORD,
-    XonLim: std.os.windows.WORD,
-    XoffLim: std.os.windows.WORD,
-    ByteSize: std.os.windows.BYTE,
-    Parity: std.os.windows.BYTE,
-    StopBits: std.os.windows.BYTE,
-    XonChar: u8,
-    XoffChar: u8,
-    ErrorChar: u8,
-    EofChar: u8,
-    EvtChar: u8,
-    wReserved1: std.os.windows.WORD,
-};
-
-const COMMTIMEOUTS = extern struct {
-    ReadIntervalTimeout: std.os.windows.DWORD = MAXDWORD,
-    ReadTotalTimeoutMultiplier: std.os.windows.DWORD = 0,
-    ReadTotalTimeoutConstant: std.os.windows.DWORD = 0,
-    WriteTotalTimeoutMultiplier: std.os.windows.DWORD = 0,
-    WriteTotalTimeoutConstant: std.os.windows.DWORD = 0,
-
-    pub fn init(timeout: u32) COMMTIMEOUTS {
-        // This mimics the default behavior of posix systems.
-        if (timeout == 0 ) return .{};
-        // This is the god awful way windows does things. Waits until data is available, or timeout.
-        // Practically, this means it waits until either the supplied buffer is full or timeout occures.
-        return .{
-            .ReadTotalTimeoutMultiplier = MAXDWORD,
-            .ReadTotalTimeoutConstant = timeout,
-        };
-    }
-};
-
-extern "kernel32" fn GetCommState(hFile: std.os.windows.HANDLE, lpDCB: *DCB) callconv(std.os.windows.WINAPI) std.os.windows.BOOL;
-extern "kernel32" fn SetCommState(hFile: std.os.windows.HANDLE, lpDCB: *DCB) callconv(std.os.windows.WINAPI) std.os.windows.BOOL;
-extern "kernel32" fn SetCommTimeouts(in_hFile: std.os.windows.HANDLE, in_lpCommTimeouts: *COMMTIMEOUTS) callconv(std.os.windows.WINAPI) std.os.windows.BOOL;
 
 test "iterate ports" {
     var it = try list();
--- src/windows/serial.zig
+++ src/windows/serial.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const serial = @import("../serial.zig");
+const MAXDWORD = std.math.maxInt(std.os.windows.DWORD);
 
 pub fn flush(file: std.fs.File, buffers: Buffers) !void {
     const result = PurgeComm(file.handle, @intFromEnum(buffers));
@@ -30,3 +31,107 @@ pub fn controlPins(file: std.fs.File, pins: serial.ControlPins) !void {
 }
 
 extern "kernel32" fn EscapeCommFunction(hFile: std.os.windows.HANDLE, dwFunc: std.os.windows.DWORD) callconv(std.os.windows.WINAPI) std.os.windows.BOOL;
+
+pub fn wConfigure(file: std.fs.File, config: serial.SerialConfig) !void {
+    var dcb = std.mem.zeroes(DCB);
+    dcb.DCBlength = @sizeOf(DCB);
+
+    if (GetCommState(file.handle, &dcb) == 0)
+        return error.WindowsError;
+
+    // std.log.err("{s} {s}", .{ dcb, flags });
+
+    dcb.BaudRate = config.baud_rate;
+
+    dcb.flags = @bitCast(DCBFlags{
+        .fParity = config.parity != .none,
+        .fOutxCtsFlow = config.handshake == .hardware,
+        .fOutX = config.handshake == .software,
+        .fInX = config.handshake == .software,
+        .fRtsControl = @as(u2, if (config.handshake == .hardware) 1 else 0),
+    });
+
+    dcb.wReserved = 0;
+    dcb.ByteSize = @intFromEnum(config.word_size);
+    dcb.Parity = switch (config.parity) {
+        .none => @as(u8, 0),
+        .even => @as(u8, 2),
+        .odd => @as(u8, 1),
+        .mark => @as(u8, 3),
+        .space => @as(u8, 4),
+    };
+    dcb.StopBits = switch (config.stop_bits) {
+        .one => @as(u2, 0),
+        .two => @as(u2, 2),
+    };
+    dcb.XonChar = 0x11;
+    dcb.XoffChar = 0x13;
+    dcb.wReserved1 = 0;
+
+    if (SetCommState(file.handle, &dcb) == 0)
+        return error.WindowsError;
+    var timeout = COMMTIMEOUTS.init(config.timeout);
+    if (SetCommTimeouts(file.handle, &timeout) == 0)
+        return error.WindowsError;
+}
+
+const DCBFlags = packed struct(u32) {
+    fBinary: bool = true, // u1
+    fParity: bool = false, // u1
+    fOutxCtsFlow: bool = false, // u1
+    fOutxDsrFlow: bool = false, // u1
+    fDtrControl: u2 = 1, // u2
+    fDsrSensitivity: bool = false, // u1
+    fTXContinueOnXoff: bool = false, // u1
+    fOutX: bool = false, // u1
+    fInX: bool = false, // u1
+    fErrorChar: bool = false, // u1
+    fNull: bool = false, // u1
+    fRtsControl: u2 = 0, // u2
+    fAbortOnError: bool = false, // u1
+    fDummy2: u17 = 0, // u17
+};
+
+/// Configuration for the serial port
+///
+/// Details: https://learn.microsoft.com/es-es/windows/win32/api/winbase/ns-winbase-dcb
+const DCB = extern struct {
+    DCBlength: std.os.windows.DWORD,
+    BaudRate: std.os.windows.DWORD,
+    flags: u32,
+    wReserved: std.os.windows.WORD,
+    XonLim: std.os.windows.WORD,
+    XoffLim: std.os.windows.WORD,
+    ByteSize: std.os.windows.BYTE,
+    Parity: std.os.windows.BYTE,
+    StopBits: std.os.windows.BYTE,
+    XonChar: u8,
+    XoffChar: u8,
+    ErrorChar: u8,
+    EofChar: u8,
+    EvtChar: u8,
+    wReserved1: std.os.windows.WORD,
+};
+
+const COMMTIMEOUTS = extern struct {
+    ReadIntervalTimeout: std.os.windows.DWORD = MAXDWORD,
+    ReadTotalTimeoutMultiplier: std.os.windows.DWORD = 0,
+    ReadTotalTimeoutConstant: std.os.windows.DWORD = 0,
+    WriteTotalTimeoutMultiplier: std.os.windows.DWORD = 0,
+    WriteTotalTimeoutConstant: std.os.windows.DWORD = 0,
+
+    pub fn init(timeout: u32) COMMTIMEOUTS {
+        // This mimics the default behavior of posix systems.
+        if (timeout == 0) return .{};
+        // This is the god awful way windows does things. Waits until data is available, or timeout.
+        // Practically, this means it waits until either the supplied buffer is full or timeout occures.
+        return .{
+            .ReadTotalTimeoutMultiplier = MAXDWORD,
+            .ReadTotalTimeoutConstant = timeout,
+        };
+    }
+};
+
+extern "kernel32" fn GetCommState(hFile: std.os.windows.HANDLE, lpDCB: *DCB) callconv(std.os.windows.WINAPI) std.os.windows.BOOL;
+extern "kernel32" fn SetCommState(hFile: std.os.windows.HANDLE, lpDCB: *DCB) callconv(std.os.windows.WINAPI) std.os.windows.BOOL;
+extern "kernel32" fn SetCommTimeouts(in_hFile: std.os.windows.HANDLE, in_lpCommTimeouts: *COMMTIMEOUTS) callconv(std.os.windows.WINAPI) std.os.windows.BOOL;
